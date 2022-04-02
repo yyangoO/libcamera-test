19c2aeee02572cd4067edd2f8ea67dcee20e8b9f
yyangoO <yyangoO@outlook.com>
2022-04-02 14:21:53 +0800

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,36 @@
 using namespace std;
 using namespace libcamera;
 
+
+static shared_ptr<Camera> camera;
+
+
 int main(void)
 {
-    static std::shared_ptr<Camera> camera;
+    string camera_id = "";
+    unique_ptr<CameraManager> cm = make_unique<CameraManager>();
+
+    cm->start();
+
+    if (cm->cameras().empty()) 
+    {
+        cout << "[no cameras were identified on the system.]" << endl;
+        cm->stop();
+        return EXIT_FAILURE;
+    }
+
+    cout << "[camera device list begin]" << endl;
+    for (auto const &camera : cm->cameras())
+    {
+        cout << camera->id() << endl;
+    }
+    cout << "[camera device list end]" << endl;
+
+    camera_id = cm->cameras()[0]->id();
+    camera = cm->get(camera_id);
+    camera->acquire();
+
+    cm->stop();
 
     return 0;
 }
